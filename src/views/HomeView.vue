c7bb8021fa1e5ecb076e2b1e5d095e82acb2172d
fakhriddin01 <a.fakhriddin@gmail.com>
2023-06-22 15:25:13 +0500

    init starter file

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { Avatar } from 'flowbite-vue'
+</script>
+
+<template>
+  <main>
+    <p class="text-red-300">Hello</p>
+      <el-button type="info">Info</el-button>
+      <el-button type="success">Success</el-button>
+      <i class='bx bx-user'></i>
+      <Avatar status="online" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" />
+  </main>
+</template>
