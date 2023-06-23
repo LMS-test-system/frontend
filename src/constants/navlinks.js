bce8b8a1e0934c73e87690f65f23f225705ee33d
fakhriddin01 <a.fakhriddin@gmail.com>
2023-06-23 11:10:34 +0500

    create aside header components

---
+++ src/constants/navlinks.js
@@ -0,0 +1,49 @@
+export const navLinks = [
+    {
+        id: 1,
+        title: "Dashboard",
+        link: "/",
+        icon: 'bx bxs-dashboard',
+    },
+    {
+        id: 2,
+        title: "O'qituvchilar",
+        link: "/teachers",
+        icon: "bx bx-user-pin",
+
+    },
+    {
+        id: 3,
+        title: "O'quvchilar",
+        link: "/students",
+        icon: "bx bxs-layer-plus"
+    },
+    {
+        id: 4,
+        title: "Guruhlar",
+        link: "/groups",
+        icon: "bx bx-calendar-alt",
+
+    },
+    {
+        id: 5,
+        title: "Fanlar",
+        link: "/subjects",
+        icon: "bx bx-book-open",
+
+    },
+    {
+        id: 6,
+        title: "Testlar",
+        link: "/tests",
+        icon: "bx bxs-hourglass",
+
+    },
+    {
+        id: 7,
+        title: "Natijalar",
+        link: "/results",
+        icon: "bx bxs-pie-chart-alt-2",
+
+    }
+]
